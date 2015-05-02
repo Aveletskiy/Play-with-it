e3af5aedb8dcce700ada5aa3ce4aa548a0af00ef
Aveletsky <ixus.van.axel@gmail.com>
2015-05-02 22:21:06 +0300

    Task Add person has been added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import models.Person;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +10,12 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("We starts with Play"));
     }
 
+    public static Result addPerson() {
+        Person person = Form.form(Person.class).bindFromRequest().get();
+        person.save();
+        return redirect(routes.Application.index());
+    }
 }
